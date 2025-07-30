d83a53572b94a23a7d2a4f07c63142946f549f71
harish saini <harishsaini1266@gmail.com>
2025-07-30 15:20:28 +0000

    container done

---
+++ javascript/datatypes.js
@@ -0,0 +1,18 @@
+let state;
+
+/* 
+number -- 1,2,3 , 2 to power 53
+string -- 'ab' , "harish"
+boolean -- true/false
+undefined --
+null--standalone value
+bigint--
+symbol--unique
+
+*/
+
+//object
+
+console.log(typeof undefined);   // undefined
+console.log(typeof null);  //object
+
