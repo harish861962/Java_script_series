6c23dab5b1e7324be7d3a62556795daa108550f5
harish saini <harishsaini1266@gmail.com>
2025-08-24 14:18:08 +0000

    fetch try catch await then methods

---
+++ advance/fetch.js
@@ -0,0 +1,24 @@
+// fetch with  try, await or catch method
+
+
+// async function getAllUsers() {
+// try {
+// const response = await fetch ('https://api.github.com/users/harish861962')
+//     const data = await response.json()
+//     console.log(data);
+// } catch (error) {
+//     console.log(error);
+// }
+// }
+// getAllUsers()
+
+
+// fetch with  then or catch method
+
+
+// fetch ('https://api.github.com/users/harish861962')
+// .then(function(response){
+//     return response.json()
+// }).then(function(data){
+//     console.log(data);
+// }).catch('error')
